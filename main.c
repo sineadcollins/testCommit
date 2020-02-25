d185cd33b823d8811f084f752c34b05aafbde86e
Sinead Collins <c3126977@uon.edu.au>
2020-02-25 14:22:58 +1100

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{//up to and including Task 10
+    float x;
+    float y;
+    printf("Enter a number: ");
+    scanf("%f", &x); //Note change of %d to %f
+    y = (x+2)/(x-1);
+    printf("y: %f\n", y);
+    return 0;
+}
